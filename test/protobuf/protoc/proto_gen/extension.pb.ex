0f50d8f47009a952a857e0344a6e41257aa2ca59
Sheng Wu <shengwu34@gmail.com>
2020-08-02 15:25:10 -0700

    Add test for field options

--- test/protobuf/protoc/proto_gen/extension.pb.ex
+++ test/protobuf/protoc/proto_gen/extension.pb.ex
@@ -7,5 +7,12 @@ defmodule Protobuf.Protoc.ExtTest.Foo do
         }
   defstruct [:a]
 
+  def descriptor do
+    # credo:disable-for-next-line
+    Elixir.Google.Protobuf.DescriptorProto.decode(
+      <<10, 3, 70, 111, 111, 18, 12, 10, 1, 97, 24, 1, 32, 1, 40, 9, 82, 1, 97>>
+    )
+  end
+
   field :a, 1, optional: true, type: :string
 end
